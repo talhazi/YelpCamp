d1eaeb00f18824ee14b3471436e7ed3118eccde6
talhazi <talhazi114@gmail.com>
2022-02-21 18:05:54 +0200

    some authorizations - campgrounds, reviews

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6208d7cad77f8f2d1c1a5eb0',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
