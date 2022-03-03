e7895c610b89de4dfc5cbf4a862ebb6339ee4fc5
talhazi <talhazi114@gmail.com>
2022-03-03 12:58:29 +0200

    handling security issues

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
+
 const userRoutes = require('./routes/users');
 const campgroundsRoutes = require('./routes/campgrounds');
 const reviewsRoutes = require('./routes/reviews');
@@ -41,6 +44,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
 
 const sessionsConfig = {
     secret: 'thisshouldbrsecret!',
